9ce40a8386aaac1886767079844cd142bc5a8cfa
oscarluu1506 <oscar@videabiz.com>
2023-04-13 15:10:30 +0700

    st

--- resources/assets/js/components/SurveyList.vue
+++ resources/assets/js/components/SurveyList.vue
@@ -24,40 +24,72 @@
 
                                     </v-text-field>
 
-                                  <v-textarea
-                                      v-model="editedItem.description"
-                                      label="Survey description"
-                                  >
+                                    <v-textarea
+                                        v-model="editedItem.description"
+                                        label="Survey description"
+                                    >
 
-                                  </v-textarea>
+                                    </v-textarea>
+                                  <v-layout row wrap>
+                                    <v-flex xs12 sm6 md4>
+                                      <v-menu
+                                          v-model="menu2"
+                                          :close-on-content-click="false"
+                                          :nudge-right="40"
+                                          lazy
+                                          transition="scale-transition"
+                                          offset-y
+                                          full-width
+                                          min-width="290px"
+                                      >
+                                        <template v-slot:activator="{ on }">
+                                          <v-text-field
+                                              v-model="date"
+                                              label="Picker without buttons"
+                                              prepend-icon="event"
+                                              readonly
+                                              v-on="on"
+                                          ></v-text-field>
+                                        </template>
+                                        <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+                                      </v-menu>
+                                    </v-flex>
+                                    <v-spacer></v-spacer>
+                                  </v-layout>
+<!--                                    <v-layout row wrap>-->
+<!--                                        <v-flex xs12 sm6 md6>-->
+<!--                                            <v-date-picker-->
+<!--                                                v-model="editedItem.start_date"-->
+<!--                                                label="Start date"-->
+<!--                                            >-->
 
-                                  <v-date-picker
-                                      v-model="editedItem.start_date"
-                                      label="Start date"
-                                  >
+<!--                                            </v-date-picker>-->
 
-                                  </v-date-picker>
+<!--                                            <v-time-picker-->
+<!--                                                v-model="editedItem.start_time"-->
+<!--                                                label="Start time"-->
+<!--                                            >-->
 
-                                  <v-time-picker
-                                      v-model="editedItem.start_time"
-                                      label="Start time"
-                                  >
+<!--                                            </v-time-picker>-->
+<!--                                        </v-flex>-->
+<!--                                    </v-layout>-->
+<!--                                    <v-layout row wrap>-->
+<!--                                        <v-flex xs12 sm6 md6>-->
+<!--                                            <v-date-picker-->
+<!--                                                v-model="editedItem.end_date"-->
+<!--                                                label="End date"-->
+<!--                                            >-->
 
-                                  </v-time-picker>
+<!--                                            </v-date-picker>-->
 
-                                  <v-date-picker
-                                      v-model="editedItem.end_date"
-                                      label="End date"
-                                  >
+<!--                                            <v-time-picker-->
+<!--                                                v-model="editedItem.end_time"-->
+<!--                                                label="End time"-->
+<!--                                            >-->
 
-                                  </v-date-picker>
-
-                                  <v-time-picker
-                                      v-model="editedItem.end_time"
-                                      label="End time"
-                                  >
-
-                                  </v-time-picker>
+<!--                                            </v-time-picker>-->
+<!--                                        </v-flex>-->
+<!--                                    </v-layout>-->
                                 </v-flex>
                             </v-layout>
                         </v-container>
@@ -114,6 +146,8 @@
         name: 'survey-list',
         data () {
             return {
+              date: new Date().toISOString().substr(0, 10),
+              menu2: false,
                 surveys: [],
                 page: 1,
                 pageLength: 1,
