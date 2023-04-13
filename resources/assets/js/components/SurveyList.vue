3d8215990a75a01be6f98af2cd875fdafe71541f
oscarluu1506 <oscar@videabiz.com>
2023-04-13 15:16:25 +0700

    st

--- resources/assets/js/components/SurveyList.vue
+++ resources/assets/js/components/SurveyList.vue
@@ -31,7 +31,30 @@
 
                                     </v-textarea>
                                   <v-layout row wrap>
-                                    <v-flex xs12 sm6 md4>
+                                    <v-flex xs12 sm6 md6>
+                                      <v-menu
+                                          v-model="menu1"
+                                          :close-on-content-click="false"
+                                          :nudge-right="40"
+                                          lazy
+                                          transition="scale-transition"
+                                          offset-y
+                                          full-width
+                                      >
+                                        <template v-slot:activator="{ on }">
+                                          <v-text-field
+                                              v-model="date1"
+                                              label="Start date"
+                                              prepend-icon="event"
+                                              readonly
+                                              v-on="on"
+                                          ></v-text-field>
+                                        </template>
+                                        <v-date-picker v-model="date1" @input="menu1 = false"></v-date-picker>
+                                      </v-menu>
+                                    </v-flex>
+                                    <v-spacer></v-spacer>
+                                    <v-flex xs12 sm6 md6>
                                       <v-menu
                                           v-model="menu2"
                                           :close-on-content-click="false"
@@ -40,21 +63,19 @@
                                           transition="scale-transition"
                                           offset-y
                                           full-width
-                                          min-width="290px"
                                       >
                                         <template v-slot:activator="{ on }">
                                           <v-text-field
-                                              v-model="date"
-                                              label="Picker without buttons"
+                                              v-model="time1"
+                                              label="Start time"
                                               prepend-icon="event"
                                               readonly
                                               v-on="on"
                                           ></v-text-field>
                                         </template>
-                                        <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+                                        <v-time-picker v-model="time1" @input="menu2 = false"></v-time-picker>
                                       </v-menu>
                                     </v-flex>
-                                    <v-spacer></v-spacer>
                                   </v-layout>
 <!--                                    <v-layout row wrap>-->
 <!--                                        <v-flex xs12 sm6 md6>-->
@@ -146,8 +167,14 @@
         name: 'survey-list',
         data () {
             return {
-              date: new Date().toISOString().substr(0, 10),
+              date1: new Date().toISOString().substr(0, 10),
+              menu1: false,
+              time1: '',
               menu2: false,
+              // date2: '',
+              // menu3: false,
+              // time2: '',
+              // menu4: false,
                 surveys: [],
                 page: 1,
                 pageLength: 1,
